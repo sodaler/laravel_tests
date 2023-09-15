8265141e0f0a500b108e9781c7ff0e6f54d0c4ff
sodaler <misha_lector87@mail.ru>
2023-09-15 13:27:03 +0400

    refactoring

--- tests/Feature/Http/Controllers/SitesControllerTest.php
+++ tests/Feature/Http/Controllers/SitesControllerTest.php
@@ -14,6 +14,37 @@ class SitesControllerTest extends TestCase
     /** @test */
     public function it_create_sites(): void
     {
+        $this->withoutExceptionHandling();
+
+        $user = UserFactory::new()->create();
+
+        $response = $this->followingRedirects()
+            ->actingAs($user)
+            ->post(route('sites.store'),
+                [
+                    'name' => 'Google',
+                    'url' => 'https://google.com'
+                ]
+            );
+
+        // make sure the site exists in the db
+        $site = Site::first();
+        $this->assertEquals(1, Site::count());
+        $this->assertEquals('Google', $site->name);
+        $this->assertEquals('https://google.com', $site->url);
+        $this->assertNull($site->is_online);
+        $this->assertEquals($user->id, $site->user->id);
+
+        // check routes page
+        $response->assertSeeText('Google');
+        $this->assertEquals(route('sites.show', $site),
+            url()->current());
+
+    }
+
+    /** @test */
+    public function it_only_allows_authenticated_users_to_create_sites()
+    {
         // make a post request to a route to create a site
         $response = $this->followingRedirects()
             ->post(route('sites.store'),
@@ -29,12 +60,5 @@ class SitesControllerTest extends TestCase
         // redirect ro login page
         $response->assertSeeText('Laravel');
         $this->assertEquals(route('login'), url()->current());
-
-    }
-
-    /** @test */
-    public function it_only_allows_authenticated_users_to_create_sites()
-    {
-
     }
 }
