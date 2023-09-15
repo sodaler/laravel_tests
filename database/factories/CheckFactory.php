4a97be03798767235f28d3f9bf8eedd150668e27
sodaler <misha_lector87@mail.ru>
2023-09-15 12:12:51 +0400

    setting up app

---
+++ database/factories/CheckFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Check>
+ */
+class CheckFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'response_status' => rand(200, 204),
+            'response_content' => $this->faker->randomHtml(),
+            'elapsed_time' => rand(1, 1000),
+
+        ];
+    }
+}
---
+++ database/migrations/2023_09_15_074519_create_checks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('checks', function (Blueprint $table) {
+            $table->id();
+            $table->integer('response_status');
+            $table->text('response_content');
+            $table->integer('elapsed_time');
+            $table->foreignIdFor(\App\Models\Check::class)
+                ->nullable()
+                ->constrained()
+                ->nullOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('checks');
+    }
+};
